a315d0c1f4bf5593f4754bbc28aee12089d7d3e8
Dylan Dunne <dylanadunne@gmail.com>
2016-06-08 15:41:40 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-//var api_path = 'http://www.stands4.com/services/v2/poetry.php?uid=5155&tokenid=JSR1kuuz7VsY4Stq'
-// parse xml
+'use strict';
 
-var captionLength = 0;
+var captionLength = 0,
+    captionEl = $('.typing-text'),
+    caption = "Ummmm, how do I describe a beautiful women????",
+    i = 0;
 
-$(document).ready(function() {
-    captionEl = $('.typing-text');
-    caption = "Ummmm, how do I describe a beautiful women????"
-    type();
-    showMainPage();
-});
-
-
+type();
+showMainPage();
 $('#generate-line').click(romanceGenerator);
+toggleResults();
+
 
 
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
-    if(captionLength < caption.length+1) {
+    if (captionLength < caption.length + 1) {
         setTimeout('type()', 50);
     } else {
         captionLength = 0;
@@ -25,27 +23,50 @@ function type() {
 }
 
 function showMainPage() {
-    $(".fa-heart").click(function(){
+    $(".fa-heart").click(function() {
         $(".intro-screen").hide();
         $(".main-page").show();
     })
 }
 
-
-
 function romanceGenerator() {
-    input = $('#query-term').val();
+    $('.toggle-arrows').show();
+    var input = $('#query-term').val();
     $.ajax({
         type: 'GET',
         url: "http://www.stands4.com/services/v2/poetry.php?uid=5155&tokenid=JSR1kuuz7VsY4Stq&term=" + input,
         dataType: "xml",
         success: function(data) {
-            $(data).find('result').each( function(){
-            var line = $(this).find('poem').text();
-            var poet = $(this).find('poet').text();
-            $('.poem-lines').append('<li class="poem">' + line + '</li>');
-            $('.poem-lines').append('<li class="poet">' + poet + '</li>');
+            var json = $.xml2json(data);
+            var poems = json['#document'].results.result;
+            $.each(poems, function(key, value) {
+                var poemSelection = value.poem.split('\n')[0];
+                if ($('.poem-lines li').length < 5) {
+                    $('.poem-lines').append('<li></h1><p class="poem-content">' + poemSelection + '</p><h5 class="poem-poet">' + '- ' + value.poet + '</h5></li>');
+                    $('li:first-child').addClass('active');
+                    console.log(value.poem);
+                } else {
+                    console.log('done');
+                }
             });
         }
     });
 };
+
+
+function toggleResults() {
+    $("#right-arrow").click(function() {
+        if ($('.active').length > 0) {
+            $('.active').removeClass('active').next().addClass('active');
+        } else {
+            $('li:first').addClass('active');
+        }
+    });
+    $("#left-arrow").click(function() {
+        if ($('.active').length > 0) {
+            $('.active').removeClass('active').prev().addClass('active');
+        } else {
+            $('li:first').addClass('active');
+        }
+    });
+}
